c830c556424612aea3b473295f0699aad31e6786
Rudrakshi <sonirudrakshi99@gmail.com>
2022-09-10 16:55:05 +0530

    added catalan number code

--- Tree/catalan_number.cpp
+++ Tree/catalan_number.cpp
@@ -1,2 +1,28 @@
 //DP
 // count no. of BSTs that can be formed using N nodes numbered from 1,2,3,4....n
+
+
+#include <iostream>
+using namespace std;
+ 
+unsigned long int catalanDP(unsigned int n)
+{
+    unsigned long int catalan[n + 1];
+ 
+    catalan[0] = catalan[1] = 1;
+ 
+    for (int i = 2; i <= n; i++) {
+        catalan[i] = 0;
+        for (int j = 0; j < i; j++)
+            catalan[i] += catalan[j] * catalan[i - j - 1];
+    }
+ 
+    return catalan[n];
+}
+ 
+int main()
+{
+    for (int i = 0; i < 10; i++)
+        cout << catalanDP(i) << " ";
+    return 0;
+}
