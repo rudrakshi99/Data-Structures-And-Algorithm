ffb83d01e49657980de86c81328a77fb7a576bdf
Rudrakshi <sonirudrakshi99@gmail.com>
2023-01-04 19:02:52 +0530

    added optimised balanced bst

--- Tree/Binary_Search_tree/IS_Balanced.cpp
+++ Tree/Binary_Search_tree/IS_Balanced.cpp
@@ -69,6 +69,24 @@ bool is_balnced(node* root){
 		return false;
 }
 
+//time complexity O(N)
+int isBalanced(node* root)
+{
+    if (root == NULL)
+        return 0;
+    int lh = isBalanced(root->left);
+    if (lh == -1)
+        return -1;
+    int rh = isBalanced(root->right);
+    if (rh == -1)
+        return -1;
+ 
+    if (abs(lh - rh) > 1)
+        return -1;
+    else
+        return max(lh, rh) + 1;
+}
+
 int main(){
 
 	node* root = Create_BST();
